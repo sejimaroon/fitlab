185694501e56c95d89e3f08203ce4bc6f748e44c
sejimaroon <39391127z@gmail.com>
2025-03-07 17:18:40 +0900

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
     <Router>
       <div className="min-h-screen bg-gray-50">
         <Navbar />
-        <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+        <main className="max-w-7xl mx-auto px-4 sm:px-6 pb-6 lg: py-8 pb-8">
           <Routes>
             <Route path="/" element={<RootRedirect />} />
             <Route path="/home" element={<Home />} />
