de1777362a1731977861b5e1a93857d1f9a2797c
sejimaroon <39391127z@gmail.com>
2025-03-07 16:36:22 +0900

    config-port

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  server: {
+    host: '0.0.0.0',
+    port: Number(process.env.PORT) || 5173, // `PORT` を数値に変換
+    strictPort: true,
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: Number(process.env.PORT) || 5173,
   },
 });
